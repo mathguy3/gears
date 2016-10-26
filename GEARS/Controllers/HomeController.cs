85e0c615068109cf2e560a950f54fbcb6ae1c076
ethansikes <ethansikes@letu.edu>
2016-10-25 22:20:45 -0500

    Added Models Partially implemented some Partially used some in the Controller

--- GEARS/Controllers/HomeController.cs
+++ GEARS/Controllers/HomeController.cs
@@ -1,30 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using GEARS.Models;
+using System;
 using System.Web.Mvc;
 
 namespace GEARS.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(String username, String password)
+        {
+            LoginInfo newLoginInfo = new LoginInfo(username, password);
+
+            return View(newLoginInfo);
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(String session, String type, String degreeGroup, 
+            String tuitCode, int year)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Course newCourse = new Course(session, type, degreeGroup, tuitCode, year);
+            return View(newCourse);
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult EditData()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        [HttpPost]
+        public ActionResult EditData(String session, String type, String degreeGroup,
+            String tuitCode, int year, int dueDate, int dueTime, int gradDueDate, int gradDueTime)
+        {
+            Course newCourse = new Course(session, type, degreeGroup, tuitCode, year);
+            DueDateTime newDueDateTime = new DueDateTime(dueDate, dueTime, gradDueDate, gradDueTime);
+
+            return View(newDueDateTime);
+        }
+
+        [HttpGet]
+        public ActionResult Emails()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Emails(int i)
+        {
+            EmailInfo newEmailInfo = new EmailInfo();
+
+            return View(newEmailInfo);
+        }
+
+        [HttpGet]
+        public ActionResult Preview()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Preview(int i)
+        {
+            PreviewInfo newPreviewInfo = new PreviewInfo();
+
+            return View(newPreviewInfo);
         }
     }
 }
---
+++ GEARS/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GEARS.Models
+{
+    public class Course
+    {
+        public String Session, Type, DegreeGroup, TuitCode;
+        public int Year;
+       
+        public Course()
+        {
+        }
+
+        public Course(String session, String type, String degreeGroup, String tuitCode, int year)
+        {
+            Session = session;
+            Type = type;
+            DegreeGroup = degreeGroup;
+            TuitCode = tuitCode;
+            Year = year;
+        }
+    }
+}
